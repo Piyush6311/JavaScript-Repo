590476a7bd2cd34da35b7b1efd11b757172d2800
Piyush6311 <140244370+Piyush6311@users.noreply.github.com>
2023-12-03 10:06:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";
+//  alert(3+3)    //we are using node js , not browser
+
+console.log(3+3) ;console.log("Hitesh")
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+let name = "piyush"
+let age = 18
+let loggedIn = false
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean ==> true/false
+//null =>standalone value 
+//undefined=> when value is not assign
+//symbol ==> unique 
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)       //object
+
+
+
+
