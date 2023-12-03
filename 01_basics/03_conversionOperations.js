bd9210a9f96954871210f1d099dc19ef6a12c54e
Piyush6311 <140244370+Piyush6311@users.noreply.github.com>
2023-12-03 10:23:07 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score =false    //NaN
+
+
+console.log(typeof score)
+
+let valueInNumber =Number(score)
+console.log(valueInNumber) //NaN is special type we should check 
+
+// "33" => 33
+// "33abc" =>NaN
+// true =>1;false =>0
+
+let isLoggedIn = 0
+
+let booleanIsLogedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLogedIn)
+
+// 1 => true
+// 0 => false
+// "piyush" => true
+
+let someNumber=33
+
+let StringNumber =String(someNumber)
+
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+
