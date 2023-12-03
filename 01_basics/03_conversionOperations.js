3bec2f428a93299eef952dec14c29dcbfc145ff5
Piyush6311 <140244370+Piyush6311@users.noreply.github.com>
2023-12-03 12:47:15 +0000

    dararype

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,30 +1,47 @@
-let score =false    //NaN
+// let score =false    //NaN
 
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber =Number(score)
-console.log(valueInNumber) //NaN is special type we should check 
+// let valueInNumber =Number(score)
+// console.log(valueInNumber) //NaN is special type we should check 
 
-// "33" => 33
-// "33abc" =>NaN
-// true =>1;false =>0
+// // "33" => 33
+// // "33abc" =>NaN
+// // true =>1;false =>0
 
-let isLoggedIn = 0
+// let isLoggedIn = 0
 
-let booleanIsLogedIn = Boolean(isLoggedIn)
+// let booleanIsLogedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLogedIn)
+// console.log(booleanIsLogedIn)
 
-// 1 => true
-// 0 => false
-// "piyush" => true
+// // 1 => true
+// // 0 => false
+// // "piyush" => true
 
-let someNumber=33
+// let someNumber=33
 
-let StringNumber =String(someNumber)
+// let StringNumber =String(someNumber)
 
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+//******************************************Operations ************************************
+
+let value =3
+let NegValue=-value
+console.log(NegValue)
+
+console.log(2+2)
+console.log(2**3)
+console.log(2%3)
+
+//----------------------------------
+
+// stack memory(primitive) and heap memory(Non primitive)
+
+let myYoutubeName="hiteshChoudhry"
+
 
 
